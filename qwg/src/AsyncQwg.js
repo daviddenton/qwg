3e13344226e0cc424e9d3ada610bcd8dec1fda80
David Denton <denton.david@gmail.com>
2012-10-20 17:04:54 +0100

    dave - removed event listener after response

--- qwg/src/AsyncQwg.js
+++ qwg/src/AsyncQwg.js
@@ -1,9 +1,10 @@
 function AsyncQwg() {
     function sendMessage(spec, callback) {
-        window.addEventListener('message', function (event) {
+        var listener = function (event) {
             callback(event.data.response);
-            window.removeEventListener('message', this);
-        });
+            window.removeEventListener('message', listener);
+        };
+        window.addEventListener('message', listener);
         chrome.extension.getBackgroundPage().document.getElementById("sandboxFrame").contentWindow.postMessage(spec, '*');
     }
 
