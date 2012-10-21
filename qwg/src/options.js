0e2227e6dddd157a9ce4d3e6fb6dd9c75f1af755
David Denton <denton.david@gmail.com>
2012-10-21 09:58:20 +0100

    dave - added save message

--- qwg/src/options.js
+++ qwg/src/options.js
@@ -2,16 +2,19 @@ $(document).ready(function () {
     var storage = new ChromeStorage(DefaultData);
 
     var editor = new CodeMirror($("#code")[0], {
-        mode:       "javascript",
-        lineNumbers:true
+        mode:         "javascript",
+        lineNumbers:  true,
+        matchBrackets:true
+    });
+
+    $("#save").click(function () {
+        storage.save(editor.getValue(), function () {
+            $("#message").text("saved").fadeOut(3000);
+        });
     });
 
     storage.load(function (schema) {
         editor.setValue(schema);
-    });
-
-    $("#save").click(function () {
-        storage.save(editor.getValue());
     });
 });
 
