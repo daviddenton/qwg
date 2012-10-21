84038a494aa570c2d74d30b6ada0f41ea2152c2e
David Denton <denton.david@gmail.com>
2012-10-21 15:20:51 +0100

    dave - fixed message on subsequent saves

--- qwg/src/options.js
+++ qwg/src/options.js
@@ -9,17 +9,19 @@ $(document).ready(function () {
 
     var memo;
     $("#save").click(function () {
-        storage.save(editor.getValue(), function () {
-            $("#message").text("saved").fadeOut(3000);
+        var newConfig = editor.getValue();
+        storage.save(newConfig, function () {
+            memo = newConfig;
+            $("#message").text("saved").show().fadeOut(3000);
         });
     });
 
     $("#reset").click(function () {
-        editor.setValue(DefaultData());
+        editor.setValue(memo);
     });
 
-    $("#cancel").click(function () {
-        editor.setValue(memo);
+    $("#defaults").click(function () {
+        editor.setValue(DefaultData());
     });
 
     storage.load(function (schema) {
