7b6708b2b5da73ca4cf5599e292145d928f011d7
David Denton <denton.david@gmail.com>
2012-10-24 23:37:37 +0100

    dave - release scripts

--- qwg/release.sh
+++ qwg/release.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -e
+#
+# Purpose: Package the Qwg extension and upload it to github
+
+if test $# -ne 1; then
+  echo "Usage: release.sh <version>"
+  exit 1
+fi
+
+version=$1
+
 ./tools/crxmake.sh src qwg.pem
-mv src.crx artifacts/qwg.crx
+
+mv src.crx artifacts/qwg-$version.crx
+
+ruby ./tools/upload-to-github.rb daviddenton daviddenton/qwg artifacts/qwg-$version.crx 'uploaded version $version'
