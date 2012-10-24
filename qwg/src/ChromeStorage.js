d760b3ddbe8af60799931d93cc55d07b383a8985
David Denton <denton.david@gmail.com>
2012-10-24 07:38:46 +0100

    dave - removed sync storage

--- qwg/src/ChromeStorage.js
+++ qwg/src/ChromeStorage.js
@@ -1,12 +1,12 @@
 function ChromeStorage(defaultDataFactory) {
     return {
         load:function (callback) {
-            chrome.storage.sync.get("qwgDataAsString", function (stored) {
+            chrome.storage.local.get("qwgDataAsString", function (stored) {
                 callback(stored.qwgDataAsString ? stored.qwgDataAsString : defaultDataFactory());
             });
         },
         save:function (updated, callback) {
-            chrome.storage.sync.set({"qwgDataAsString":updated}, function () {
+            chrome.storage.local.set({"qwgDataAsString":updated}, function () {
                 callback && callback();
             });
         }
